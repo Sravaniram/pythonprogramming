42ffcc240f90b667d65434ff5c2799b6f363a665
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-08-24 21:14:25 +0530

    Create armstrong range.py

---
+++ armstrong range.py
@@ -0,0 +1,13 @@
+lower = int(input())
+upper = int(input())
+ 
+for num in range(lower,upper + 1):
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num == sum:
+       print(num)
