5edc4e48f5d07b05221e2f3d575fa39685d7aa84
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-09-01 16:40:59 +0530

    Create number to word.py

---
+++ number to word.py
@@ -0,0 +1,12 @@
+y=int(input())
+if(y==1):print("One")
+elif(y==2):print("Two")
+elif(y==3):print("Three")
+elif(y==4):print("Four")
+elif(y==5):print("Five")
+elif(y==6):print("Six")
+elif(y==7):print("Seven")
+elif(y==8):print("Eight")
+elif(y==9):print("Nine")
+else:print("Ten")
+
