90201cf4e652484e9d4cd1ff5903144fb03a9940
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-09-02 18:12:21 +0530

    Update list contains number and alpha.py

--- list contains number and alpha.py
+++ list contains number and alpha.py
@@ -1,10 +1,10 @@
-a1=str(input())
+b1=str(input())
 s=0
 d=0
-for x in a1:
+for x in b1:
     if(x.isalpha()):
         s=s+1
-for y in a1:
+for y in b1:
     if(y.isnumeric()):
         d=d+1
 if(d>0 and s>0):
