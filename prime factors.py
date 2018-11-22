d432619a9506ce603343707535cf0ef0f2782425
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-11-22 12:25:55 +0530

    Update prime factors.py

--- prime factors.py
+++ prime factors.py
@@ -9,8 +9,8 @@ for x in range(2,a+1):
         if(c==2):
             y.append(x)
 for i in range(0,len(y)):
-    if(i<len(y)):
-        print(y[i],end=(' '))
-    else:
+    if(i==len(y)-1):
         print(y[i])
+    else:
+        print(y[i],end=(' '))
         
