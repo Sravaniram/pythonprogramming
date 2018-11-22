2e2b427a0b8c8e7e01fce84069ec863f21acb486
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-11-22 12:19:04 +0530

    Create prime factors.py

---
+++ prime factors.py
@@ -0,0 +1,16 @@
+a=int(input())
+y=[]
+for x in range(2,a+1):
+    if(a%x==0):
+        c=0
+        for i in range(1,x+1):
+            if(x%i==0):
+                c=c+1
+        if(c==2):
+            y.append(x)
+for i in range(0,len(y)):
+    if(i<len(y)):
+        print(y[i],end=(' '))
+    else:
+        print(y[i])
+        
