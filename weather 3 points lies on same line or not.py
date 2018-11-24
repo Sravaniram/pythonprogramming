0aa932b4c79eb9e4b8b08226cef27056b4f7a39c
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-11-24 12:56:57 +0530

    Update weather 3 points lies on same line or not.py

--- weather 3 points lies on same line or not.py
+++ weather 3 points lies on same line or not.py
@@ -3,5 +3,7 @@ x2,y2=map(int,input().split())
 x3,y3=map(int,input().split())
 if((x1==x1==x3) or (y1==y2==y3)):
     print("yes")
+elif(((y2-y1)/(x2-x1))==((y3-y1)/(x3-x1))):
+    print("yes")
 else:
     print("no")
