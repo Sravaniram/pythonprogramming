36b8ec36adae579b0537c262ebf18290f5a2b99e
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-11-27 11:12:13 +0530

    Create index and number.py

---
+++ index and number.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=input().split()
+c=len(b)
+p=[]
+for i in range(0,c):
+	if i%2==0:
+		b[i]=int(b[i])
+		if b[i]%2!=0:
+			p.append(b[i])
+	else:
+		b[i]=int(b[i])
+		if b[i]%2==0:
+			p.append(b[i])
+s=" ".join(map(str,p))
+print(s)
