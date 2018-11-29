aa9bf266c06909664c237415020cfdc4f05517e0
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-11-29 12:39:11 +0530

    Create assending order of a string.py

---
+++ assending order of a string.py
@@ -0,0 +1,14 @@
+s=input().split()
+a=[]
+x=0
+l=int(len(s))
+for x in range(0,len(s)):
+    a.append(s[x])
+a.sort()
+for x in a:
+    if(x==a[len(s)-1]):
+        print(x)
+    else:
+        print(x,end=(" "))
+    
+    
