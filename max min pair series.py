62e6b7be5bf3df3209e015780e1f6a7356243fa7
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-10 19:42:43 +0530

    Create max min pair series.py

---
+++ max min pair series.py
@@ -0,0 +1,15 @@
+n=int(input())
+l1=input().split()
+l=[]
+for x in l1:
+    l.append(int(x))
+l.sort()
+a=[]
+k=int(len(l)/2)
+for x in range(0,k):
+    a.append(l[len(l)-1-x])
+    a.append(l[x])
+if(len(l)%2!=0):
+    a.append(l[k])
+s=" ".join(map(str,a))
+print(s)
