c5e24a8dd3acc9d3000175d82596da9ac8e4eb84
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-15 12:53:57 +0530

    Update prime numbers in assending order.py

--- prime numbers in assending order.py
+++ prime numbers in assending order.py
@@ -6,7 +6,9 @@ for x in range(2,n):
             break
     else:
         k.append(x)
-            
-s=" ".join(map(str,k))
-print(s)
-            
+for i in range(0,len(k)):
+    if(i==len(k)-1):
+        print(k[i],end=(""))
+    else:
+        print(k[i],end=(" "))
+   
