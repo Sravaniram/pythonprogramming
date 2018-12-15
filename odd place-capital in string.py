29b0bbdea8e1de591087b6ceed9265ae9724b243
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-15 12:21:12 +0530

    Update odd place-capital in string.py

--- odd place-capital in string.py
+++ odd place-capital in string.py
@@ -1,9 +1,15 @@
 n=input()
+y=0
 k=[]
 for x in range(0,len(n)):
-    if(x%2!=0):
+    if(n[x].isspace()):
+        y=y
         k.append(n[x])
+    elif(y%2!=0):
+        k.append(n[x])
+        y=y+1
     else:
         k.append(n[x].upper())
-for x in k:
-    print(x,end=(""))
+        y=y+1
+s="".join(map(str,k))
+print(s)
