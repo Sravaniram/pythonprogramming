74ed10929e3e85ec358b76b5fb009611b9168cf2
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-15 12:37:38 +0530

    Update reversing a each element of a list.py

--- reversing a each element of a list.py
+++ reversing a each element of a list.py
@@ -1,7 +1,10 @@
-import sys
 n=input().split()
 k=[]
 for x in n:
     k.append(x[::-1])
-sys.stdout.write(k)
+for i in range(0,len(k)):
+    if(i==len(k)-1):
+        print(k[i],end=(""))
+    else:
+        print(k[i],end=(" "))
     
