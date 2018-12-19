da8ac7cf6f4fc89d310cdc66d61e5c9af24bba97
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-19 20:46:09 +0530

    Create median-removing elements.py

---
+++ median-removing elements.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=[]
+s=input().split()
+for x in s:
+    l.append(int(x))
+x=len(l)
+while(len(l)>1):
+    if(len(l)%2==0):
+        k=l[int(len(l)/2)]+l[int(len(l)/2)-1]
+        print(int(k/2))
+        x=int(len(l)/2)
+        del(l[x-1:x+1])
+    else:
+        print(l[int(len(l)/2)])
+        del(l[int(len(l)/2)])
+        
+        
