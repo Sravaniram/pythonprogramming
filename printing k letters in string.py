46e6400b604cbac528b6ab0aa2bfb99fbc70b32c
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-22 19:19:55 +0530

    Update printing k letters in string.py

--- printing k letters in string.py
+++ printing k letters in string.py
@@ -2,9 +2,16 @@ n=input().split()
 x=int(n[1])
 y=str(n[0])
 for i in range(0,len(y)-x+1):
-    for j in range(i,i+x):
-        if(j<i+x-1):
-            print(y[j],end=(""))
-        else:
-            print(y[j],end=(" "))
-    
+    if(i<len(y)-x):
+        for j in range(i,i+x):
+            if(j<i+x-1):
+                print(y[j],end=(""))
+            else:
+                print(y[j],end=(" "))
+    else:
+        for j in range(i,i+x):
+            if(j<i+x-1):
+                print(y[j],end=(""))
+            else:
+                print(y[j],end=(""))
+        
