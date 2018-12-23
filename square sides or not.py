c56c8e4fadccbe3b2e54b3dc70fbf5e98749a90f
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-23 22:27:24 +0530

    Create square sides or not.py

---
+++ square sides or not.py
@@ -0,0 +1,8 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+x4,y4=map(int,input().split())
+if(x1+x2+x3+x4==y1+y2+y3+y4):
+    print("yes")
+else:
+    print("no")
