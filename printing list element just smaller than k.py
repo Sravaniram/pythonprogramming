2c46040e5d1fd963613ea37924b170839c1e4238
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2018-12-25 17:30:59 +0530

    Update printing list element just smaller than k.py

--- printing list element just smaller than k.py
+++ printing list element just smaller than k.py
@@ -1,10 +1,10 @@
 m,n=map(int,input().split())
 l=input().split()
-l.sort()
-for x in range(0,len(l)-1):
-    if(int(l[x])==n):
-        print(l[x+1])
+l1=[]
+for x in l:
+    l1.append(int(x))
+l1.sort()
+for x in range(0,len(l1)):
+    if(l1[x]>n):
+        print(l1[x])
         break
-if(len(l)==1):
-    print(l[0])
-    
