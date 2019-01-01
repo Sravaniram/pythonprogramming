4971597aaab1207abe5b988dbf781ce9c2de919b
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-01-01 17:35:38 +0530

    Update suming the index ranging.py

--- suming the index ranging.py
+++ suming the index ranging.py
@@ -1,11 +1,8 @@
 n,m=map(int,input().split())
 l=list(map(int,input().split()))
-k=[]
 for x in range(0,m):
     i,j=map(int,input().split())
     s=0
-    for y in range(i,j+1):
+    for y in range(i-1,j):
         s=s+l[y]
-    k.append(s)
-for x in k:
-    print(x)
+    print(s)
