60578ffd62f6a97af0ae6d8a71fdd13212a4058f
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-01-04 15:20:24 +0530

    Create reversing a list.py

---
+++ reversing a list.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+l=l[::-1]
+s="->".join(map(str,l))
+'''for x in range(0,len(l)):
+    if(x==len(l)-1):
+        print(l[x],end=(""))
+    else:
+        print(l[x],end=("->"))'''
+print(s)
+    
