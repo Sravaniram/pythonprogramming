c61d8c38ac1c7cdad21ca58dc6625b550c578c00
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-01-04 15:11:21 +0530

    Update max sum of sub array.py

--- max sum of sub array.py
+++ max sum of sub array.py
@@ -6,5 +6,11 @@ k.append(m)
 for x in range(1,len(l)):
     m=m+l[x] 
     k.append(m)
+k.append(l[len(l)-1])
+a=l[len(l)-1]
+for y in range(len(l)-2,-1,-1):
+    a=a+l[y] 
+    k.append(a)
+    
     
 print(max(k))
