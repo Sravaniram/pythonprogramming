90f67d55eb641950a9b9e5bb13e70f5c55489eba
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-01-06 16:26:14 +0000

    Create max len of subarray.py

---
+++ max len of subarray.py
@@ -0,0 +1,12 @@
+n=int(input())
+m=list(map(int,input().split()))
+c=1
+k=[]
+for x in range(1,len(m)):
+    if(m[x]>m[x-1]):
+        c=c+1
+    else:
+        k.append(c)
+        c=1
+k.append(c)
+print(max(k))
