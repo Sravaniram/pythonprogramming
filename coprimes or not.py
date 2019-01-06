ab5f4ef3cd587f8b17414d63e46aa2b3d5afb12e
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-01-06 22:16:00 +0530

    Update coprimes or not.py

--- coprimes or not.py
+++ coprimes or not.py
@@ -1,11 +1,11 @@
 n,m=map(int,input().split())
 p=0
 if(n==2 and m==3):
-    print("yes")
+    p=p+1
 for x in range(2,max(n,m)):
     if(n%x==0 and m%x==0):
         p=p+1 
-if(p>1):
+if(p>0):
     print("yes")
 else:
     print("no")
