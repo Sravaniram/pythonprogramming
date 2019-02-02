797aea297ac0ec0298bf0aa689d8fa990ddfb5b3
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-02-02 20:17:04 +0530

    Create ai>aj>ak if i<j<k count.py

---
+++ ai>aj>ak if i<j<k count.py
@@ -0,0 +1,10 @@
+n=int(input())
+c=0
+m=list(map(int,input().split()))
+for x in range(0,n-2):
+    for y in range(x+1,n-1):
+        for z in range(y+1,n):
+            if(m[x]>m[y]>m[z]):
+                c=c+1
+    
+print(c)
