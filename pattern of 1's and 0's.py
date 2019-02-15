ba3e055a3441afc6f8596f410b973123e197eb17
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-02-15 20:52:20 +0530

    Update pattern of 1's and 0's.py

--- pattern of 1's and 0's.py
+++ pattern of 1's and 0's.py
@@ -1,24 +1,21 @@
 o,z=map(int,input().split())
 a=[]
-if(o==1 and z==2):
-    print("011")
-else:
-    if(z==o or z+1==o):
-        for x in range(0,z+o):
-            if(x%2==0):
-                a.append(1)
-            else:
-                a.append(0)
-        s="".join(map(str,a))
-        print(s[:z+o])
-    elif(z==(2*o) or z==(2*o)+2 or z==(2*o)+1):
-        for x in range(0,z+1):
-            a.append(1) 
-            x=x+1 
+if(z==o or z+1==o):
+    for x in range(0,z+o):
+        if(x%2==0):
             a.append(1)
-            x=x+1 
+        else:
             a.append(0)
-        s="".join(map(str,a))
-        print(s[:z+o])
-    else:
-        print("-1")
+    s="".join(map(str,a))
+    print(s[:z+o])
+elif(z==(2*o) or z==(2*o)+2 or z==(2*o)+1):
+    for x in range(0,z+1):
+        a.append(1) 
+        x=x+1 
+        a.append(1)
+        x=x+1 
+        a.append(0)
+    s="".join(map(str,a))
+    print(s[:z+o])
+else:
+    print("-1")
