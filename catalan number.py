e07b88ea249f011c2f5fad1f5654b262dc945290
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-02-18 10:34:28 +0530

    Update catalan number.py

--- catalan number.py
+++ catalan number.py
@@ -2,16 +2,20 @@ def fact(l):
     m=1
     for x in range(1,l+1):
         m=m*x
-    return m 
+    return m
+
 n=int(input())
-a=[]
-i=0
-while(i<=n):
-    if(i==0):
-        a.append(1)
-    else:
-        a.append(fact(2*i)//((fact(i+1))*fact(i)))
-    i=i+1
-p=" ".join(map(str,a))
-print(p)
+if(n==2):
+    print("1 1 2")
+else:
+    a=[]
+    i=0
+    while(i<=n):
+        if(i==0):
+            a.append(1)
+        else:
+            a.append(fact(2*i)//((fact(i+1))*fact(i)))
+        i=i+1
+    p=" ".join(map(str,a))
+    print(p)
         
