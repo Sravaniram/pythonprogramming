c833b9ca5babf64ea4e105156036cb0d868a2dc4
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-02-18 10:29:26 +0530

    Create catalan number.py

---
+++ catalan number.py
@@ -0,0 +1,17 @@
+def fact(l):
+    m=1
+    for x in range(1,l+1):
+        m=m*x
+    return m 
+n=int(input())
+a=[]
+i=0
+while(i<=n):
+    if(i==0):
+        a.append(1)
+    else:
+        a.append(fact(2*i)//((fact(i+1))*fact(i)))
+    i=i+1
+p=" ".join(map(str,a))
+print(p)
+        
