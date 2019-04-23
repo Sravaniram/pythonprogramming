a9557ce3da8eaa525cf0471da8419a7467cd8384
SRAVANI.KATRAGADDA <42571507+Sravaniram@users.noreply.github.com>
2019-04-23 18:18:03 +0530

    Create position of 2nd one form left to right.py

---
+++ position of 2nd one form left to right.py
@@ -0,0 +1,11 @@
+n,m=map(int,input().split())
+k=bin(n*m)
+print(k)
+c,p=0,1
+for x in range(2,len(k)):
+    c=c+1
+    if(k[x]=='1'):
+        if(p==2):
+            print(c)
+            break
+        p=p+1
